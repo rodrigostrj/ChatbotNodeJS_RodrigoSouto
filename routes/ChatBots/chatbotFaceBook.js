bd9f8a217e646996a96d1eb61ce1a8b8428f389e
Rodrigo Souto <Rodrigo Souto@RODRIGOSOUTO>
2017-05-20 18:38:52 -0300

    ajuetes no fluxo de resposta do Chat Facebook

--- routes/ChatBots/chatbotFaceBook.js
+++ routes/ChatBots/chatbotFaceBook.js
@@ -1,11 +1,38 @@
 var securityModule = require(__base + '/modules/module-security');
 
+var webhookPath = "/webhook/facebook/";
+
 module.exports = function(app)
 {
-    app.get("/webhook/facebook/", securityModule.ensureAuthorizedFacebook, function(req, res)
+    app.get(webhookPath, securityModule.ensureAuthorizedFacebook, function(req, res)
     {
         var facebookReturnValue = req.query['hub.challenge'];
          return res.status(200).send(facebookReturnValue);
+    });
+
+    app.post(webhookPath, function(req,res)
+    {
+        var data = req.body;
+        if(!(data && data.object == "page"))   
+        {
+            return res.status(400).send("object 'page' not found");
+        }
+
+        data.entry.forEach(function(entry)
+        {
+            var pageID = entry.id;
+            var eventTime = entry.time;
+
+            data.messaging.forEach(function(event)
+            {
+                if(event.message)
+                {
+                    console.log(event.message);
+                }
+            });
+        });
+
+
     });
 }
 
