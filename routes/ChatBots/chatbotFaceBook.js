16bba66fa8d3def1cf08e3e21b1279c23238b757
Rodrigo Souto <Rodrigo Souto@RODRIGOSOUTO>
2017-05-20 20:31:31 -0300

    ajustando parametro event para tratamento de resposta

--- routes/ChatBots/chatbotFaceBook.js
+++ routes/ChatBots/chatbotFaceBook.js
@@ -25,7 +25,7 @@ module.exports = function(app)
             {
                 if (event.message && event.message.text)
                 {
-                    sendMessage(event.message.text);
+                    sendMessage(event);
                 }
             });
         });
