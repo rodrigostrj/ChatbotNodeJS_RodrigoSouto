4c002eb46b4ba3b84210484c2eade5b08f35db67
Rodrigo Souto <Rodrigo Souto@RODRIGOSOUTO>
2017-05-20 18:15:06 -0300

    ajustes de segurança - facebook

--- routes/ChatBots/chatbotFaceBook.js
+++ routes/ChatBots/chatbotFaceBook.js
@@ -4,7 +4,8 @@ module.exports = function(app)
 {
     app.get("/webhook/facebook/", securityModule.ensureAuthorizedFacebook, function(req, res)
     {
-         return res.status(200).json({message: 'OK'});
+        var facebookReturnValue = req.query['hub.challenge'];
+         return res.status(200).send(facebookReturnValue);
     });
 }
 
